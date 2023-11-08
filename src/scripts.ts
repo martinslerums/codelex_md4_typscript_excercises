6fc0b32db1bebf1cc26e856f67bb05fb406ace86
martleru <68188509+martleru@users.noreply.github.com>
2023-11-08 13:49:20 +0200

    almost done

--- src/scripts.ts
+++ src/scripts.ts
@@ -615,4 +615,175 @@ function removeObjectProperty(object: Object2): Object2 {
       }
     }  
     return newObject;
-  }
+}
+
+
+
+// Write a function that takes two objects as arguments
+// Unfortunately, the property 'b' in the second object has the wrong key
+// should be named 'd' instead
+// Merge both objects and correct the wrong property name
+// Return the resulting object
+// It should have the properties 'a', 'b', 'c', 'd', and 'e'
+
+const objectMerger = (objectOne: Object2, objectTwo: Object2): Object2 => {
+   
+    // const { b, ...restObjectTwo } = objectTwo; => shorthard for:
+    const restObjectTwo: Object2 = {};
+    for (const key in objectTwo) {
+        if (key !== 'b') {
+          restObjectTwo[key] = objectTwo[key];
+        }
+      }
+    
+    const mergedObject: Object2 = {
+    ...objectOne,
+    ...restObjectTwo,
+    d: objectTwo.b
+    };
+    return mergedObject;
+}
+
+
+
+// Write a function that takes an object (a) and a number (b) as arguments
+// Multiply all values of 'a' by 'b'
+// Return the resulting object
+
+const multiplyObject = (object: Object2, b: number): Object2 => {
+    const multipliedObject: Object2 = {}
+    for(const key in object) {
+        multipliedObject[key] = object[key] * b
+    }
+    return multipliedObject
+}
+
+
+
+// Write a function that takes an object as argument
+// Some of the property values contain empty strings
+// Replace empty strings and strings that contain only whitespace with null values
+// Return the resulting object
+
+
+const whiteToNull = (object: Object2): Object2 => {
+    for (const key in object) {
+        const noWhiteSpaces = object[key].trim();
+        if (noWhiteSpaces === '') {
+            object[key] = null;
+        }
+    }
+    return object;
+}
+
+
+
+// Write a function that takes an object as argument containing properties with personal information
+// Extract firstName, lastName, size, and weight if available
+// If size or weight is given transform the value to a string
+// Attach the unit cm to the size
+// Attach the unit kg to the weight
+// Return a new object with all available properties that we are interested in
+
+const someHardObject = (object: Object2): Object2 => {
+    const { fn, ln, size, weight } = object;
+
+    // const fn = object.fn;
+    // const ln = object.ln;
+    // const size = object.size;
+    // const weight = object.weight;
+
+    const interestingProperties: Object2 = {
+        fn,
+        ln,
+    };
+
+    if (size !== undefined) {
+        interestingProperties.size = `${size}cm`;
+    }
+
+    if (weight !== undefined) {
+        interestingProperties.weight = `${weight}kg`;
+    }
+
+    return interestingProperties;
+}
+
+
+
+// Write a function that takes an array of objects and a string as arguments
+// Add a property with key 'continent' and value equal to the string to each of the objects
+// Return the new array of objects
+// Tip: try not to mutate the original array
+
+
+const arrayStringObject = (objectArray: Object2 [], str: string): Object2 [] => {
+    
+    const updatedObjects: Object2[] = objectArray.map(
+        obj => {
+            const newObject: Object2 = { ...obj };
+            newObject.continent = str;
+            return newObject;
+        });
+    return updatedObjects;
+}
+
+
+
+// Write a function that takes an array of numbers as argument
+// Convert the array to an object
+// It should have a key for each unique value of the array
+// The corresponding object value should be the number of times the key occurs within the array
+
+const numberArrayObject = (numbers: number []): Object2 => {
+    const occurancesObject: Object2 = {};
+
+    numbers.forEach(number => {
+        if (occurancesObject[number]) {
+            occurancesObject[number]++;
+        } else {
+            occurancesObject[number] = 1;
+        }
+    });
+    return occurancesObject;
+}
+
+
+
+// Write a function that takes two date instances as arguments
+// It should return true if the dates are equal
+// It should return false otherwise
+
+const compareDates = (dateOne: Date, dateTwo: Date): boolean => {
+    return dateOne.getTime() === dateTwo.getTime()
+}
+
+
+
+// Write a function that takes two date instances as argument
+// It should return the number of days that lies between those dates
+
+const differenceDates = (dateOne: Date, dateTwo: Date): number => {
+    const milliseconds = Math.abs(dateTwo.getTime() - dateOne.getTime());
+    const days = Math.round(milliseconds/((24 * 60 * 60 * 1000)));
+    return days
+}
+
+
+
+// Write a function that takes two date instances as argument
+// It should return true if they fall on the exact same day
+// It should return false otherwise
+
+const sameDay = (dateOne: Date, dateTwo: Date): boolean => {
+
+    if(dateOne.getFullYear() === dateTwo.getFullYear() &&
+    dateOne.getMonth() === dateTwo.getMonth() &&
+    dateOne.getDate() === dateTwo.getDate()) {
+        return true
+    }
+    return false
+    
+}
+
+console.log(sameDay(new Date('2000/01/01'), new Date('2000/01/01')))
